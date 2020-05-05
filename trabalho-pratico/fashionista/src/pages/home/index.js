736e24124cd8066ea38db495e661eda8ddb53e93
obraia <bryan22czo@gmail.com>
2020-05-05 16:20:55 -0300

    Componentes destaques conceitualmente pronto

--- trabalho-pratico/fashionista/src/pages/home/index.js
+++ trabalho-pratico/fashionista/src/pages/home/index.js
@@ -1,36 +1,58 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import 'typeface-righteous';
-import { Container, Header, Trends, Title, TrendsList, TrendsListItem } from './styles';
+import { Container, Header, Title, Trends, ArrowButton, TrendsList, TrendsListItem } from './styles';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 import ProductCard from '../../components/productCard';
 
-import productImage from './img/product.jpg';
+import product01 from './img/product01.jpg';
+import product02 from './img/product02.jpg';
+import product03 from './img/product03.jpg';
+import product04 from './img/product04.jpg';
 
 function Home() {
+
+    const trendList = useRef(null);
+    const [trendScrollValue, setTrendScrollValue] = useState(0);
+
+    useEffect(() => {
+        trendList.current.scrollTo(trendScrollValue, 0);
+    }, [trendScrollValue]);
 
     return (
         <Container>
             <Header />
+            <Title><span>Em destaque</span></Title>
             <Trends>
-                <Title><span>Em destaque</span></Title>
-                <TrendsList>
+                <ArrowButton onClick={() => setTrendScrollValue(trendScrollValue - 283)}>
+                    <FontAwesomeIcon icon={faChevronLeft} />
+                </ArrowButton>
+                <TrendsList ref={trendList}>
                     <TrendsListItem>
-                        <ProductCard image={productImage} isInPromotion={true} promotionPercent={30} />
+                        <ProductCard image={product01} isInPromotion={true} promotionPercent={30} />
                     </TrendsListItem>
                     <TrendsListItem>
-                        <ProductCard image={productImage} isInPromotion={true} promotionPercent={50} />
+                        <ProductCard image={product02} isInPromotion={true} promotionPercent={50} />
                     </TrendsListItem>
                     <TrendsListItem>
-                        <ProductCard image={productImage} isInPromotion={false} promotionPercent={0} />
+                        <ProductCard image={product03} isInPromotion={false} promotionPercent={0} />
                     </TrendsListItem>
                     <TrendsListItem>
-                        <ProductCard image={productImage} isInPromotion={false} promotionPercent={0} />
+                        <ProductCard image={product04} isInPromotion={false} promotionPercent={0} />
                     </TrendsListItem>
                     <TrendsListItem>
-                        <ProductCard image={productImage} isInPromotion={true} promotionPercent={15} />
+                        <ProductCard image={product01} isInPromotion={true} promotionPercent={15} />
+                    </TrendsListItem>
+                    <TrendsListItem>
+                        <ProductCard image={product01} isInPromotion={true} promotionPercent={15} />
                     </TrendsListItem>
                 </TrendsList>
+                <ArrowButton onClick={() => setTrendScrollValue(trendScrollValue + 283)}>
+                    <FontAwesomeIcon icon={faChevronRight} />
+                </ArrowButton>
             </Trends>
         </Container>
     );
--- trabalho-pratico/fashionista/src/pages/home/styles.js
+++ trabalho-pratico/fashionista/src/pages/home/styles.js
@@ -8,6 +8,7 @@ export const Container = styled.article`
     padding-top: 60px;
     display: flex;
     flex-direction: column;
+    align-items: center;
 `;
 
 export const Header = styled.div`
@@ -22,13 +23,6 @@ export const Header = styled.div`
     background-position: 0px 60px;
     background-repeat: no-repeat;
     background-attachment: fixed;
-`;
-
-export const Trends = styled.section`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
 `;
 
 export const Title = styled.h1`
@@ -59,11 +53,61 @@ export const Title = styled.h1`
     }
 `;
 
+export const Trends = styled.section`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const ArrowButton = styled.button`
+    width: 50px;
+    height: 50px;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    font-size: 48px;
+    color: ${props => props.theme.colors.textSecondary};
+    cursor: pointer;
+    transition: color .2s;
+
+    &:hover{
+        transition: color .2s;
+        color: ${props => lighten(0.2, props.theme.colors.textSecondary)};
+    }
+
+    &:active{
+        transition: font-size .2s;
+        font-size: 52px;
+    }
+`;
+
 export const TrendsList = styled.ul`
-    width: 1132px;
+    width: 1107px;
     list-style: none;
     display: flex;
     overflow-x: scroll;
+
+    margin: 0 25px;
+    margin-bottom: 50px;
+
+    scroll-behavior: smooth;
+
+    ::-webkit-scrollbar {
+        scrollbar-width: 150px;
+		width: 8px;
+		height: 8px;
+	}
+	::-webkit-scrollbar-track {
+        border-radius: 10px;
+		background-color: ${props => lighten(0.3, props.theme.colors.secondary)};
+	}
+	::-webkit-scrollbar-thumb {
+        width: 100px;
+        height: 100px;
+		border-radius: 10px;
+        background-color: ${props => props.theme.colors.secondary};	
+	}
 `;
 
 export const TrendsListItem = styled.li`
